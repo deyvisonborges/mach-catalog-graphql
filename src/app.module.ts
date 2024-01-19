77d125f2ce9ad8913527cf3350d23e4f2b47ab63
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-01-18 23:06:53 -0300

    feat: configure schema first to graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import path from 'path'
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true
+      autoSchemaFile: path.resolve(process.cwd(), '/src/schemq.gql')
     })
   ],
   controllers: [AppController],
