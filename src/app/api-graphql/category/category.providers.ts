12a4f0e854e1a727cd56cc959e9ca7d70805d8b6
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-02-05 02:00:41 -0300

    add resolvers to category

--- src/app/api-graphql/category/category.providers.ts
+++ src/app/api-graphql/category/category.providers.ts
@@ -1,5 +1,8 @@
+import { DeleteCategoryService } from 'src/core/artifacts/category/service/delete-category.service'
 import { CategoryInMemoryRepository } from '../../../core/artifacts/category/repository/category.in-memory.repository'
 import { CreateCategoryService } from '../../../core/artifacts/category/service/create-category.service'
+import { RetrieveASingleCategory } from 'src/core/artifacts/category/service/find-category-by-id.service'
+import { FindAllCategoriesService } from 'src/core/artifacts/category/service/find-all-categories.service'
 
 const repositories = [
   {
@@ -13,6 +16,24 @@ const services = [
     provide: CreateCategoryService,
     useFactory: (categoryRepo: CategoryInMemoryRepository) =>
       new CreateCategoryService(categoryRepo),
+    inject: [CategoryInMemoryRepository]
+  },
+  {
+    provide: DeleteCategoryService,
+    useFactory: (categoryRepo: CategoryInMemoryRepository) =>
+      new DeleteCategoryService(categoryRepo),
+    inject: [CategoryInMemoryRepository]
+  },
+  {
+    provide: RetrieveASingleCategory,
+    useFactory: (categoryRepo: CategoryInMemoryRepository) =>
+      new RetrieveASingleCategory(categoryRepo),
+    inject: [CategoryInMemoryRepository]
+  },
+  {
+    provide: FindAllCategoriesService,
+    useFactory: (categoryRepo: CategoryInMemoryRepository) =>
+      new FindAllCategoriesService(categoryRepo),
     inject: [CategoryInMemoryRepository]
   }
 ]
--- src/app/api-graphql/category/graphql/category.resolver.ts
+++ src/app/api-graphql/category/graphql/category.resolver.ts
@@ -1,16 +1,36 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql'
+import { Inject } from '@nestjs/common'
 import { CategoryOutput } from './category.output'
 import { CategoryInput } from './category.input'
-import { Inject } from '@nestjs/common'
 import { CreateCategoryService } from '../../../../core/artifacts/category/service/create-category.service'
+import { DeleteCategoryService } from '../../../../core/artifacts/category/service/delete-category.service'
+import { RetrieveASingleCategory } from '../../../../core/artifacts/category/service/find-category-by-id.service'
+import { FindAllCategoriesService } from '../../../../core/artifacts/category/service/find-all-categories.service'
 
 @Resolver(() => CategoryOutput)
 export class CategoryResolver {
-  @Inject()
-  private readonly createCategoryService: CreateCategoryService
+  @Inject() private readonly createCategoryService: CreateCategoryService
+  @Inject() private readonly removeCategoryService: DeleteCategoryService
+  @Inject() private readonly findOneCategoryService: RetrieveASingleCategory
+  @Inject() private readonly findAllCategoryService: FindAllCategoriesService
 
   @Mutation(() => CategoryOutput)
   async createCategory(@Args('input') input: CategoryInput) {
     return await this.createCategoryService.execute(input)
+  }
+
+  @Mutation(() => CategoryOutput)
+  async removeCategory(@Args('id', { type: () => String }) id: string) {
+    return await this.removeCategoryService.execute({ id })
+  }
+
+  @Query(() => CategoryOutput)
+  async findOne(@Args('input') id: string): Promise<CategoryOutput> {
+    return await this.findOneCategoryService.execute({ id })
+  }
+
+  @Query(() => [CategoryOutput])
+  async findAllCategories(): Promise<CategoryOutput[]> {
+    return await this.findAllCategoryService.execute()
   }
 }
