4a2615fb3cdda5883e24a130c1ff43efa2a1c2db
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-02-10 23:56:08 -0300

    core product: working in factory implementation

--- src/core/artifacts/product/product.factory.ts
+++ src/core/artifacts/product/product.factory.ts
@@ -1,64 +1,32 @@
-// /* eslint-disable @typescript-eslint/no-empty-interface */
-// // Define uma interface base para a configuração de produtos
-// interface ProductConfig {}
+import { BaseModelProps } from 'src/core/common/base/model.base'
+import {
+  VirtualProductModel,
+  VirtualProductModelProps
+} from '../virtual-product/virtual-product.model'
+import {
+  SimpleProductModel,
+  SimpleProductModelProps
+} from '../simple-product/simple-product.model'
 
-// // Define uma classe abstrata para representar um modelo de produto
-// abstract class ProductModel {
-//   constructor(public config: ProductConfig) {}
-//   abstract create(config: ProductConfig): ProductModel
-//   // Adicione outros métodos necessários aqui
-// }
+type Product = SimpleProductModel | VirtualProductModel
+type ProductTypeConstant = 'simple' | 'virtual' | 'configurable'
 
-// // Implementa a classe concreta para produtos simples
-// class SimpleProductModel extends ProductModel {
-//   size: string
-//   constructor(config: SimpleProductConfig) {
-//     super(config)
-//     this.size = config.size
-//     // Implementação específica para produtos simples
-//   }
-//   create(config: SimpleProductConfig): SimpleProductModel {
-//     return new SimpleProductModel(config)
-//   }
-//   // Implementa outros métodos necessários aqui
-// }
-
-// // Implementa a classe concreta para produtos virtuais
-// class VirtualProductModel extends ProductModel {
-//   constructor(config: VirtualProductConfig) {
-//     super(config)
-//     // Implementação específica para produtos virtuais
-//   }
-//   create(config: VirtualProductConfig): VirtualProductModel {
-//     return new VirtualProductModel(config)
-//   }
-//   // Implementa outros métodos necessários aqui
-// }
-
-// // Define a configuração específica para produtos simples
-// interface SimpleProductConfig extends ProductConfig {
-//   size: string
-// }
-
-// // Define a configuração específica para produtos virtuais
-// type VirtualProductConfig = ProductConfig
-
-// // Define a classe da fábrica de produtos
-// export class ProductFactory {
-//   static createProduct(
-//     type: 'simple' | 'virtual',
-//     config: SimpleProductConfig | VirtualProductConfig
-//   ): ProductModel {
-//     if (type === 'simple') {
-//       return new SimpleProductModel(config as SimpleProductConfig)
-//     } else if (type === 'virtual') {
-//       return new VirtualProductModel(config as VirtualProductConfig)
-//     } else {
-//       throw new Error('Tipo de produto inválido')
-//     }
-//   }
-// }
-
-// const a = ProductFactory.createProduct('simple', {
-//   size: "'"
-// })
+export class ProductFactory {
+  public createProduct<T extends ProductTypeConstant>(
+    type: T,
+    config: T extends 'simple'
+      ? Omit<SimpleProductModelProps, keyof BaseModelProps>
+      : T extends 'virtual'
+      ? Omit<VirtualProductModelProps, keyof BaseModelProps>
+      : never
+  ): Product {
+    switch (type) {
+      case 'simple':
+        return new SimpleProductModel(config as SimpleProductModelProps)
+      case 'virtual':
+        return new VirtualProductModel(config as VirtualProductModelProps)
+      default:
+        throw new Error('Invalid product type')
+    }
+  }
+}
---
+++ src/core/artifacts/product/product.spec.ts
@@ -0,0 +1,11 @@
+// import { ProductFactory } from './product.factory'
+
+// describe('', () => {
+//   const product = new ProductFactory().createProduct('simple', {
+//     categoriesIds: new Map(),
+//     costPrice: 10,
+//     description: 'descr',
+
+//   })
+//   product.
+// })
