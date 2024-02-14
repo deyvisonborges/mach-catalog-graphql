128453bcfd372d4445d103d646c01e84ef3aaeda
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-02-14 14:54:12 -0300

    working in create product

--- src/core/artifacts/product/service/create-product.ts
+++ src/core/artifacts/product/service/create-product.ts
@@ -12,6 +12,9 @@ import { SimpleProductRepositoryContract } from '../../simple-product/repository
 import { ProductFactory } from '../product.factory'
 import { BaseModelProps } from 'src/core/common/base/model.base'
 import { VirtualProductRepositoryContract } from '../../virtual-product/repository/virtual-product.repository.contract'
+import { ProductRepositoryContract } from '../repository/product.repository.contract'
+import { HttpException, HttpStatus } from '@nestjs/common'
+import { ProductTypeRepositoryContract } from '../../product-type/repository/product-type.repository.contract'
 
 type Input = {
   type: ProductTypeConstant
@@ -23,13 +26,39 @@ type Input = {
 export class CreateProductService implements BaseServiceContract<Input, null> {
   constructor(
     private readonly productFactory: ProductFactory, // Alterado para aceitar qualquer tipo de produto
+    private readonly productRepository: ProductRepositoryContract,
     private readonly simpleProductRepository: SimpleProductRepositoryContract,
-    private readonly virtualProductRepository: VirtualProductRepositoryContract
+    private readonly virtualProductRepository: VirtualProductRepositoryContract,
+    private readonly productTypeRepository: ProductTypeRepositoryContract
   ) {}
 
   async execute(input: Input): Promise<null> {
     const product = this.productFactory.createProduct(input.type, input.config)
 
+    // verifica se o produto ja existe através do sku informado
+    const hasProductWithSku = await this.productRepository.findProductBySku(
+      product.sku
+    )
+
+    if (hasProductWithSku)
+      throw new HttpException(
+        { message: 'Product already exists' },
+        HttpStatus.BAD_REQUEST
+      )
+
+    // valida se o tipo do produto informado, é valido
+    const hasProductType = await this.productTypeRepository.findByName(
+      input.type
+    )
+
+    // se o tipo de produto informado, não for válido, retorna um erro
+    if (!hasProductType)
+      throw new HttpException(
+        { message: 'Invalid product type' },
+        HttpStatus.BAD_REQUEST
+      )
+
+    // cria o produto de acordo com o tipo informado
     if (input.type === 'simple' && product instanceof SimpleProductModel) {
       await this.simpleProductRepository.createOne({
         material: product.material,
